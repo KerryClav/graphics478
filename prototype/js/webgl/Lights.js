5ace5504ffda7b1f6497d09faba43fcdb158d410
Kerry Clavadetscher <kerry.clavadetscher@yale.edu>
2013-05-07 20:11:47 -0400

    lights don't go into the ground anymore yaay!

--- prototype/js/webgl/Lights.js
+++ prototype/js/webgl/Lights.js
@@ -5,8 +5,8 @@ jarlightidx = 0;
 // a light is a firefly
 function Light(name){
 	this.id = name;
-	this.startposition = [Math.random()*10-5,0.2,Math.random()*10-5];
-	this.position = this.startposition;
+	this.startposition = [Math.random()*10-5,0.25,Math.random()*10-5];
+	this.position = this.startposition.slice(0);
 	this.ambient = [1.0,1.0,1.0];
 	this.diffuse = [0.3,0.3,0.2];
 	this.specular = [0.0,0.0,0.0];
@@ -16,9 +16,9 @@ function Light(name){
 	this.is_caught = [0.0, 1.0];
 	
 	// used to move uncaught fireflies
-	this.dr = [Math.random()*0.01,Math.random()*0.01,Math.random()*0.01];
-	this.w = [2 * Math.PI * Math.random()*0.01,
-		  2 * Math.PI * Math.random()*0.01, 2 * Math.PI * Math.random()*0.01];
+	this.dr = [Math.random()*0.2,Math.random()*0.1,Math.random()*0.2];
+	this.w = [2 * Math.PI * Math.random()*0.02,
+		  2 * Math.PI * Math.random()*0.02, 2 * Math.PI * Math.random()*0.02];
 }
 
 Light.prototype.setIsCaught = function(p){
@@ -93,10 +93,11 @@ var Lights = {
 	},
 	
 	catchLight: function(idx, pos){
-		this.list[idx].startposition = pos;
-		this.list[idx].position = pos;
-		console.log('position was ' + pos);
-		this.list[jarlightidx].is_caught[1] += 1.0;
+		console.log('position was ' + this.list[idx].startposition);
+		this.list[idx].startposition = pos.slice(0);
+		this.list[idx].position = pos.slice(0);
+		console.log('new pos is ' + pos);
+		this.list[jarlightidx].is_caught[1] += 5.0;
 		return;		
 	},
 	
@@ -105,9 +106,13 @@ var Lights = {
 		for(var i = 0, max = this.list.length; i < max; i+=1){
 			// if the light is not caught then move it
 			if(this.list[i].is_caught[0] == 0.0) {
+				console.log('start position of ' + i + ' is ' + this.list[i].startposition);
+				console.log('position of ' + i + ' is ' + this.list[i].position);
 				for(var j = 0; j < 3; j+=1){
 					this.list[i].position[j] = this.list[i].startposition[j] +
-						this.list[i].dr[j] * Math.sin(this.list[i].w[j] * t);
+						(this.list[i].dr[j] * 0.5 *
+						(Math.sin(this.list[i].w[j] * Math.sin(this.list[i].w[j]* .01 * t) * t) +
+						Math.cos(this.list[i].w[j]*t)));
 				}
 			}
 		}
